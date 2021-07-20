0b49063dec494693c0624c75c18448a7d97007ae
felixJarteaga <farteaga21791@gmail.com>
2021-07-20 21:41:06 +0200

    terminanado seccion 8

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) {}
 
-  ngOnInit(): void {
+  buscar(termino: string) {
+    this.termino = termino;
+    this.hayError = false;
+
+    this.paisService.buscarCapital(this.termino).subscribe(
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      },
+      (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
-
 }
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -9,19 +10,28 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private paisService: PaisService) {}
 
-  buscar() {
-    console.log(this.termino);
+  buscar(termino: string) {
+    this.termino = termino;
     this.hayError = false;
+
     this.paisService.buscarPais(this.termino).subscribe(
-      (resp) => {
-        console.log(resp);
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
       },
       (err) => {
         this.hayError = true;
+        this.paises = [];
       }
     );
+  }
+
+  sugerencias(termino: string) {
+    this.hayError = false;
+    //TODO crear sugerencias
   }
 }
