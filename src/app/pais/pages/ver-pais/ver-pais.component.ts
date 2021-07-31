3dec5c51ec0f98b13482cd01b6662973a5488ae2
felixJarteaga <farteaga21791@gmail.com>
2021-07-31 13:38:42 +0200

    fin Seccion 8

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,18 +10,26 @@ import { PaisService } from '../../services/pais.service';
   styles: [],
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private paisService: PaisService
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params) => {
-      console.log(params.id);
+    this.activatedRoute.params
+      .pipe(
+        switchMap((param) => this.paisService.getPasisPorAlpha(param.id)),
+        tap(console.log)
+      )
+      .subscribe((pais) => (this.pais = pais));
+    // this.activatedRoute.params.subscribe((params) => {
+    //   console.log(params.id);
 
-      this.paisService.getPasisPorAlpha(params.id).subscribe((pais) => {
-        console.log(pais);
-      });
-    });
+    //   this.paisService.getPasisPorAlpha(params.id).subscribe((pais) => {
+    //     console.log(pais);
+    //   });
+    // });
   }
 }
